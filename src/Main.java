aae12d1094ad13b6f099dc1ee6c2fd6cec06b3cd
Praneeth Appikatla <pappikatla@>
2017-03-16 11:56:12 -0400

    added name

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Tom White");
+        System.out.println("Praneeth Appikatla");
         System.out.println("Evan Duffy");
         System.out.println("Andrew Nagal");
         System.out.println("John Bieber");
